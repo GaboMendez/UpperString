2ca0221d52b32a8713a07310ebf46c9c9d5ea15f
GaboSchool <1071470@est.intec.edu.do>
2019-11-13 23:58:16 -0400

    finish

--- TendenciasCase/TendenciasCase/Program.cs
+++ TendenciasCase/TendenciasCase/Program.cs
@@ -10,14 +10,41 @@ namespace TendenciasCase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Escriba un texto: ");
-            string mensaje = Console.ReadLine();
+            bool x = true;
+            while (x)
+            {
+                Console.Clear();
+                Console.WriteLine("Escriba un Texto o -1 para Salir: ");
+                string mensaje = Console.ReadLine();
+                Console.WriteLine();
+                if (mensaje.Equals("-1"))
+                {
+                    Console.WriteLine("Pase Buenas Noches :DDDD!!!!");
+                    Console.ReadKey();
+                    x = false;
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Su Texto en Mayuscula es: ");
+                    Console.WriteLine(StringToUpper(mensaje));
 
-            string mensajeUpper = mensaje.ToUpper();
+                    Console.WriteLine(".");
+                    Console.ReadKey();
+                }
 
-            Console.WriteLine(mensajeUpper);
+            }
 
-            Console.ReadKey();
+            //string mensajeUpper = mensaje.ToUpper();
+
+            //Console.WriteLine(mensajeUpper);
+
+            //Console.ReadKey();
+        }
+
+        public static string StringToUpper (string mensaje)
+        {
+            return mensaje.ToUpper();
         }
     }
 }
